a17f927c80dee81a38970a0a3715ac3e0729bc33
Stiven <stiven.ballshi12@gmail.com>
2024-01-07 15:31:03 -0500

    finalizing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import hole from "./assets/hole.png";
+import mole from "./assets/mole.png";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [holes, setHoles] = useState<boolean[]>(new Array(9).fill(true));
+  const [score, setScore] = useState(0);
+
+  const moleVisibility = (idx: number, isVisible: boolean) => {
+    const newHoles = [...holes];
+    newHoles[idx] = isVisible;
+    setHoles(newHoles);
+  };
+
+  const handleClick = (idx: number) => {
+    if (holes[idx]) return;
+    moleVisibility(idx, true);
+    setScore(score + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomPos = Math.floor(Math.random() * holes.length);
+      moleVisibility(randomPos, false);
+
+      setTimeout(() => {
+        moleVisibility(randomPos, true);
+      }, 1000);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [holes]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="title">Your score is : {score}</h1>
+
+      <div className="img-container">
+        {holes.map((isHole: any, idx: number) => (
+          <img
+            onClick={() => handleClick(idx)}
+            key={idx}
+            src={isHole ? hole : mole}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
